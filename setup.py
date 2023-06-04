c411c4e1ef02b03bf7dcf294b64ffe9899320ab2
rahulsamsum007 <135415621+rahulsamsum007@users.noreply.github.com>
2023-06-04 14:54:54 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='streamlit-image-captioning-app',
-    version='1.0',
+    name='streamlit-app',
+    version='0.1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'streamlit',
         'torch',
-        'transformers',
-        'Pillow',
+        'transformers'
     ],
+    entry_points={
+        'console_scripts': [
+            'streamlit-app = app:main'
+        ]
+    },
 )
